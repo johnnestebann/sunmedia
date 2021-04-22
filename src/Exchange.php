1747bbf98808c927339ad849157e67e43888e0aa
John Astete <john.astete@lmvsoftware.com>
2021-04-22 17:21:05 -0300

    update

--- src/Exchange.php
+++ src/Exchange.php
@@ -8,23 +8,31 @@ class Exchange implements ExchangeInterface
     /** @var CampaignInterface[]|null */
     private array $campaigns;
 
-    public function match(UserInterface $user): ?CampaignInterface
+    public function match(?UserInterface $user): ?CampaignInterface
     {
         $campaign = null;
+
+        if (null !== $user) {
+
+        }
 
         return $campaign;
     }
 
-    public function addCampaign(CampaignInterface $campaign): void
+    public function addCampaign(?CampaignInterface $campaign): void
     {
-        $this->campaigns[] = $campaign;
+        if (null !== $campaign) {
+            $this->campaigns[] = $campaign;
+        }
     }
 
-    public function removeCampaign(CampaignInterface $campaign): void
+    public function removeCampaign(?CampaignInterface $campaign): void
     {
-        $this->campaigns = array_filter($this->campaigns, function(CampaignInterface $camp) use ($campaign) {
-            return $campaign->id() !== $camp->id();
-        });
+        if (null !== $campaign && count($this->campaigns) > 0) {
+            $this->campaigns = array_filter($this->campaigns, function (CampaignInterface $camp) use ($campaign) {
+                return $campaign->id() !== $camp->id();
+            });
+        }
     }
 
     public function campaigns(): array
@@ -32,21 +40,25 @@ class Exchange implements ExchangeInterface
         return $this->campaigns;
     }
 
-    public function getCampaignById(int $id): ?CampaignInterface
+    public function getCampaignById(?int $id): ?CampaignInterface
     {
         $campaign = null;
-        $found = false;
-        $i = 0;
 
-        do {
+        if (null !== $id && count($this->campaigns) > 0) {
 
-            if ($this->campaigns[$i]->id() === $id) {
-                $found = true;
-                $campaign = $this->campaigns[$id];
-            }
+            $found = false;
+            $i = 0;
 
-            $i++;
-        } while (!$found && $i < count($this->campaigns));
+            do {
+
+                if ($this->campaigns[$i]->id() === $id) {
+                    $found = true;
+                    $campaign = $this->campaigns[$id];
+                }
+
+                $i++;
+            } while (!$found && $i < count($this->campaigns));
+        }
 
         return $campaign;
     }
